19cb11af4581ceb3cb3bff10eed4f652cf0cdf3e
menglan <menglan_yan@outlook.com>
2025-10-03 21:19:52 +0100

    Initial commit: setup project

---
+++ src/test/java/com/github/menglanyan/inventory_management/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.menglanyan.inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
