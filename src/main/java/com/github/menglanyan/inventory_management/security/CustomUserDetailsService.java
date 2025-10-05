b08ee3b49d8c8f6877f91a98b739dd7168fa25f7
menglan <menglan_yan@outlook.com>
2025-10-05 16:28:29 +0100

    feat: add Spring Security configuration and filters

---
+++ src/main/java/com/github/menglanyan/inventory_management/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.github.menglanyan.inventory_management.security;
+
+import com.github.menglanyan.inventory_management.entities.User;
+import com.github.menglanyan.inventory_management.exceptions.NotFoundException;
+import com.github.menglanyan.inventory_management.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException("User Email Not Found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
