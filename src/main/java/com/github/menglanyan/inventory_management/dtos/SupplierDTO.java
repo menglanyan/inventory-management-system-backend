2fabfbcec0071ae8292e559db52b4976bfbac8c1
menglan <menglan_yan@outlook.com>
2025-10-06 16:25:56 +0100

    refactor: enforce validation on fields

--- src/main/java/com/github/menglanyan/inventory_management/dtos/SupplierDTO.java
+++ src/main/java/com/github/menglanyan/inventory_management/dtos/SupplierDTO.java
@@ -2,6 +2,7 @@ package com.github.menglanyan.inventory_management.dtos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,10 @@ public class SupplierDTO {
 
     private Long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
 
+    @NotBlank(message = "Contact information is required")
     private String contactInfo;
 
     private String address;
